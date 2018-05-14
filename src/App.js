264c82ffc8f9e4ccca3f0fe5068ce79f9621a473
R Adysurya A <ri7nz.labs@gmail.com>
2018-05-14 11:07:44 +0700

    Normal Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: [] 
+  }
+  componentDidMount(){
+    console.log("ComponentDidmount Sedang Berjalan")
+    const urlFetch = fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid") 
+    console.log("Fetch data sedang berjalan") 
+    urlFetch.then(res => {
+        if( res.status === 200 )
+            return res.json()
+    }).then( resJson => {
+        console.log("Mengatur State.data")
+        this.setState({
+            data: resJson 
+        })
+    })
+  }
   render() {
+      console.log(`Render lifecycle: ${JSON.stringify(this.state)}`)
     return (
       <div className="App">
         <header className="App-header">
@@ -14,7 +33,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
