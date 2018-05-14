2fa05fac0f400465ef2b39ef9ba75ac716af3266
ri7nz <ri7nz.labs@gmail.com>
2018-05-14 17:06:28 +0700

    Update AsyncFetch.js

--- src/AsyncFetch.js
+++ src/AsyncFetch.js
@@ -1,12 +1,11 @@
 /**
  * @const  {AsyncFetch} Async Function
- * @param  {url} String 
+ * @param  {url} String URL to Fetch Resource
  * @const  {urlFetch} Object Promise from Fetch API
- * @return {Object|Array} if http.status 200 return JSON else Array 
+ * @return {Object|Array} if http.status 200 return JSON default An Array
  */
 const AsyncFetch = async (url) => {
     const urlFetch = await fetch(url)
-
     return urlFetch.status === 200 && 'json' in urlFetch ? 
         await urlFetch.json() : [] 
 }
